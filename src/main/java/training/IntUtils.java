aee55593bb1df676625b6408b668ec2047102109
pangnem <sprnd645@gmail.com>
2022-07-19 23:09:20 +0900

    라이브 코딩용 베이스 코드로 변경

--- src/main/java/training/IntUtils.java
+++ src/main/java/training/IntUtils.java
@@ -1,20 +1,19 @@
 package training;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-
-import static java.util.stream.Collectors.toList;
 
 public class IntUtils {
 
 	public static List<Integer> returnEvens(Integer... integers) {
-		return Arrays.stream(integers)
-				.filter(IntUtils::isEven)
-				.collect(toList());
-	}
+		List<Integer> evens = new ArrayList<>();
 
-	private static boolean isEven(Integer integer) {
-		return integer % 2 == 0;
-	}
+		for (Integer integer : integers) {
+			if (integer % 2 == 0) {
+				evens.add(integer);
+			}
+		}
 
+		return evens;
+	}
 }
